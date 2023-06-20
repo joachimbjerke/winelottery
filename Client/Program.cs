f6ead4f66fa3abb16af51834e31858323f934afe
Joachim Bjerke <joachim.bjerke@norgesgruppen.no>
2023-06-20 10:02:01 +0200

    update

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,24 +3,12 @@ using Client;
 using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Common.Models;
+using Client.Startup;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<LotteryModel>();
-builder.Services.AddScoped<UserModel>();
-//builder.Services.AddSingleton<ILotteryService, LotteryService>();
-var baseUri = builder.Configuration["ApiBaseUri"];
-builder.Services.AddHttpClient<ILotteryService, LotteryService>(client =>
-{
-    client.BaseAddress = new Uri(baseUri);
-});
-
-
+builder.Services.RegisterApplicationServices(builder.Configuration);
 
 await builder.Build().RunAsync();
---
+++ Client/Startup/ServiceInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+using Client.Services;
+using Common.Models;
+
+namespace Client.Startup
+{
+    public static partial class ServiceInitializer
+    {
+        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddBlazoredLocalStorage();
+            services.AddScoped<LotteryModel>();
+            services.AddScoped<UserModel>();
+            var baseUri = configuration["ApiBaseUri"];
+            services.AddHttpClient<ILotteryService, LotteryService>(client =>
+            {
+                client.BaseAddress = new Uri(baseUri);
+            });
+
+            return services;
+        }
+    }
+}
